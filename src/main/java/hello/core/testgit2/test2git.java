b146953bf9753f9d30ea26d674aeaad2e3575366
LeeSungHee_cycwell <lee98417716@daum.net>
2022-11-28 11:30:48 +0900

    git test 0.0.2

---
+++ src/main/java/hello/core/testgit2/test2git.java
@@ -0,0 +1,12 @@
+package hello.core.testgit2;
+
+/**
+ * Project: core
+ * Created by IntelliJ IDEA.
+ * User: CYWELL
+ * Date: 2022-11-28
+ * Time: 오전 11:30
+ * PackageName : hello.core.testgit2
+ */
+public class test2git {
+}
